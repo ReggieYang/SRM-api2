4d73422d31a3a265b05dc435374e131ef9079746
ReggieYang <1937730499@qq.com>
2016-11-11 01:09:13 +0800

    /

--- src/main/source/model/Followup.scala
+++ src/main/source/model/Followup.scala
@@ -8,4 +8,4 @@ import scala.beans.BeanProperty
   * Created by ReggieYang on 2016/11/6.
   */
 case class Followup(@BeanProperty var followupId: Int = Utils.NULL_ID, @BeanProperty var status: String,
-                    @BeanProperty var followupDate: String, riskId: Int)
+                    @BeanProperty var followupDate: String, @BeanProperty var riskId: Int)
